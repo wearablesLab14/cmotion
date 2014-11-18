688063eedb43ba6c77aab7a73c67878a13971b50
Bundschuh <sebastianmaxjaeger@gmail.com>
2014-11-18 18:39:11 +0100

    code style

--- Nghia/SmartPhone/src/de/freiburg/ese/cmotion/smartwatch/MainActivity.java
+++ Nghia/SmartPhone/src/de/freiburg/ese/cmotion/smartwatch/MainActivity.java
@@ -5,31 +5,20 @@ package de.freiburg.ese.cmotion.smartwatch;
 
 // import com.example.eyespeedtest.R;
 
-
-
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
 
 import android.app.Activity;
-import android.app.Application;
-import android.app.LauncherActivity.IconResizer;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -37,14 +26,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 	private boolean color = false;
 	// private View view;
 	private long lastUpdate;
-	private  float [] lastValues ;
-	UdpSender sender  = new UdpSender();
-	SendQuaternionUDPTask async ;
-	// SendQuaternionUDPTask async = new SendQuaternionUDPTask();
-
+	private float[] lastValues;
+	SendQuaternionUDPTask async;
 	Button button;
 	TextView text;
 	private float[] lastRotation;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -53,8 +40,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.activity_main);
-		//view = findViewById(R.id.textView1);
-		//view.setBackgroundColor(Color.GREEN);
+		// view = findViewById(R.id.textView1);
+		// view.setBackgroundColor(Color.GREEN);
 
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		sensorManager.registerListener(this,
@@ -63,14 +50,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 		sensorManager.registerListener(this,
 				sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
 				SensorManager.SENSOR_DELAY_NORMAL);
-		
+
 		lastUpdate = System.currentTimeMillis();
-		
-		
+
 		button = (Button) findViewById(R.id.button1);
 		text = (TextView) findViewById(R.id.textView1);
-		 
-		text.setTextSize(20);	
+
+		text.setTextSize(20);
 	}
 
 	@Override
@@ -91,56 +77,53 @@ public class MainActivity extends Activity implements SensorEventListener {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	private void getAccelerometer(SensorEvent event) throws IOException {
 		float[] values = event.values;
-		if(this.lastRotation == null || this.lastRotation.length != 5)
+		if (this.lastRotation == null || this.lastRotation.length != 5)
 			return;
-		
+
 		// beschleunigung
 		float x = values[0];
 		float y = values[1];
-		float z = values[2];	
-		
-		
+		float z = values[2];
+
 		// rotation berechnen
-	
+
 		float rotationx = this.lastRotation[0];
-		float 	rotationy= this.lastRotation[1];
-		float 	rotationz= this.lastRotation[2];
-		float[] rotation = new float[]{rotationx,rotationy,rotationz} ;
+		float rotationy = this.lastRotation[1];
+		float rotationz = this.lastRotation[2];
+		float[] rotation = new float[] { rotationx, rotationy, rotationz };
 
-//			float w = (float)Math.sqrt(1
-//					- rotation[0]*rotation[0] - rotation[1]*rotation[1] - rotation[2]*rotation[2]);
-			//In this case, the w component of the quaternion is known to be a positive number
+		// float w = (float)Math.sqrt(1
+		// - rotation[0]*rotation[0] - rotation[1]*rotation[1] -
+		// rotation[2]*rotation[2]);
+		// In this case, the w component of the quaternion is known to be a
+		// positive number
 
-			float [] q  = new float[4];
-			float [] rv = new float[]{ rotationx,rotationy,rotationz}; 
-		
+		float[] q = new float[4];
+		float[] rv = new float[] { rotationx, rotationy, rotationz };
+
 		sensorManager.getQuaternionFromVector(q, rv);
-		
+
 		float[] qq = q;
-	
-		
+
 		async = new SendQuaternionUDPTask();
-		 async.execute(qq, "192.168.0.255", 5050);
-		
-		 
-			this.text.setText(
-					// "Rotation X: " + (int)(rotationx*1000)  + "\n" + 
-					// "Rotation Y: " + ((int)(rotationy*1000) )+ "\n" + 
-					// "Rotation Z: " + ((int)(rotationz*1000) )+ "\n" + 
-					// "\n\n\n"++ 
-					qq[0] + "\n" +qq[1] + "\n" + qq[2] + "\n" +qq[3] + "\n"
-					+ "Rotation \n" +
-					"X: " + rotationx +"\nY: " + rotationy+"\nZ: " + rotationz 
-					
-					);	
+		async.execute(qq, "192.168.0.255", 5050);
+
+		this.text.setText(
+		// "Rotation X: " + (int)(rotationx*1000) + "\n" +
+		// "Rotation Y: " + ((int)(rotationy*1000) )+ "\n" +
+		// "Rotation Z: " + ((int)(rotationz*1000) )+ "\n" +
+		// "\n\n\n"++
+				qq[0] + "\n" + qq[1] + "\n" + qq[2] + "\n" + qq[3] + "\n"
+						+ "Rotation \n" + "X: " + rotationx + "\nY: "
+						+ rotationy + "\nZ: " + rotationz
+
+				);
 
 		this.lastValues = values;
 	}
-	
-
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
@@ -151,24 +134,21 @@ public class MainActivity extends Activity implements SensorEventListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}	else if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
-		{
-				getRotationMeter(event);
+		} else if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+			getRotationMeter(event);
 		}
 	}
-	
-	
 
 	private void getRotationMeter(SensorEvent event) {
-		this.lastRotation =   event.values;	
+		this.lastRotation = event.values;
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -182,6 +162,4 @@ public class MainActivity extends Activity implements SensorEventListener {
 				SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
-	
-	
 }
