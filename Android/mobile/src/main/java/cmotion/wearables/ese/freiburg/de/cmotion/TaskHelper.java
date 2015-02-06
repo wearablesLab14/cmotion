4edda8c59dd029a07d5ebe2fb010388cd340b5d0
Bundschuh <sebastianmaxjaeger@gmail.com>
2015-02-06 03:23:40 +0100

    javadoc

--- Android/mobile/src/main/java/cmotion/wearables/ese/freiburg/de/cmotion/TaskHelper.java
+++ Android/mobile/src/main/java/cmotion/wearables/ese/freiburg/de/cmotion/TaskHelper.java
@@ -4,19 +4,21 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Build;
 
+/**
+ * A helper class to allow multiple tasks to run in parallel on a pool of threads managed by AsyncTask.
+ */
 class TaskHelper {
 
-	public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task) {
-		execute(task, (P[]) null);
-	}
+    public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task) {
+        execute(task, (P[]) null);
+    }
 
-	@SuppressLint("NewApi")
-	public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task,
-			P... params) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
-		} else {
-			task.execute(params);
-		}
-	}
+    @SuppressLint("NewApi")
+    public static <P, T extends AsyncTask<P, ?, ?>> void execute(T task, P... params) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+        } else {
+            task.execute(params);
+        }
+    }
 }
